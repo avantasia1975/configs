f5d8a7a85472181e2d005585582b39e68c4f5679
a k <kaminand75@gmail.com>
2021-11-23 19:32:46 +0100

    Changes

--- config/scripts/power.sh
+++ config/scripts/power.sh
@@ -7,6 +7,6 @@ choice=$(echo "Shutdown Reboot Lock Logout" | rofi -dmenu -sep " " -theme power)
 case $choice in 
     Shutdown) systemctl poweroff;;
     Reboot) kill -15 $(pidof chrome) && reboot;;
-    Lock) dm-tool lock;;
+    Lock) ~/.config/scripts/lock.sh;; #Dm-tool lock;;
     Logout) qtile cmd-obj -o cmd -f shutdown;;
 esac
